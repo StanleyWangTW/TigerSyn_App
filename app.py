f24c108b8b8f4d566ba5639e42100db174949f8a
StanleyWangTW <x039516@gmail.com>
2023-11-06 14:28:41 +0800

    html template index

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
+@app.route("/index")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
