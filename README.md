6be609a63d09c73dc97b049c45d1cb3b1338e9c3
StanleyWangTW <x039516@gmail.com>
2023-11-06 15:14:06 +0800

    file upload form prototype

---
+++ README.md
@@ -0,0 +1,8 @@
+README
+===
+**要安裝的套件**
+```cmd
+pip install Flask
+pip install flask_wtf
+pip install WTForms
+```
--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
+app.config['UPLOAD_FOLDER'] = 'files'
+class UploadFileForm(FlaskForm):
+    file = FileField('file')
+    submit = SubmitField('Upload File')
 
-@app.route("/")
-@app.route("/index")
-def hello_world():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(
+            'files',
+            secure_filename((file.filename))))
+
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
