3b1ee20ec75cabf6975161039238f182f89844b6
StanleyWangTW <x039516@gmail.com>
2023-11-13 22:00:49 +0800

    rearrange routes

--- app.py
+++ app.py
@@ -1,80 +1,54 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+import os
 
+from flask import Flask, render_template, redirect, request, session, url_for
 import nibabel as nib
 from nilearn.image import reorder_img
 import tigersyn
 import numpy as np
 import matplotlib
-import matplotlib.pyplot as plt
 import cv2
 
-import glob
-import time
-import os
+from tools import *
 
 matplotlib.use('agg')
-
-def to_grayscale(img):
-    return ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
-
-def mask_to_3img(mask, dir, cmap):
-    plt.figure()
-    plt.imshow(np.rot90(mask[100, :, :]), cmap=cmap)
-    plt.axis('off')
-    plt.savefig(os.path.join(dir, 'sagittal_mask.jpg'), bbox_inches='tight')
-
-    plt.figure()
-    plt.imshow(np.rot90(mask[:, :, 100]), cmap=cmap)
-    plt.axis('off')
-    plt.savefig(os.path.join(dir, 'axial_mask.jpg'), bbox_inches='tight')
-
-    plt.figure()
-    plt.imshow(np.rot90(mask[:, 100, :]), cmap=cmap)
-    plt.axis('off')
-    plt.savefig(os.path.join(dir, 'coronal_mask.jpg'), bbox_inches='tight')
-
-
-if not os.path.exists('files'):
-    os.makedirs('files')
-
-if not os.path.exists('static'):
-    os.makedirs('static')
-
 app = Flask(__name__)
+
+if not os.path.isdir('uploads'):
+    os.mkdir('uploads')
+if not os.path.isdir('static'):
+    os.mkdir('static')
+if not os.path.isdir('download'):
+    os.mkdir('download')
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
 app.secret_key = 'EE304'
-app.config['UPLOAD_FOLDER'] = 'files'
+
+# if not os.path.exists('files'):
+#     os.makedirs('files')
+# if not os.path.exists('static'):
+#     os.makedirs('static')
+
+# app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/home", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         file = request.files['file']
-        if file.filename != '':
+        if '.nii' in file.filename:
             session['img_fname'] = os.path.basename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz'))
-            return redirect('/display')
-    else:
-        return render_template('index.html')
+            return redirect(url_for('show'))
+    
+    return render_template('index.html')
 
-
-@app.route("/display", methods=['GET', 'POST'])
-def display():
+@app.route("/show", methods=['GET', 'POST'])
+def show():
     if 'img_fname' in session:
         img = reorder_img(
-            nib.load(os.path.join('files', 'image.nii.gz')),
+            nib.load(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz')),
             resample='nearest').get_fdata()
-        
-        session['v1'] = int(request.form.get('sag')) if request.form.get('sag') is not None else 0
-        print(session['v1'])
 
-        sagittal_data = np.rot90(img[100, :, :])
-        cv2.imwrite(r'static\sagittal.jpg', to_grayscale(sagittal_data))
-
-        axial_data = np.rot90(img[:, :, 100])
-        cv2.imwrite(r'static\axial.jpg', to_grayscale(axial_data))
-
-        coronal_data = np.rot90(img[:, 100, :])
-        cv2.imwrite(r'static\coronal.jpg', to_grayscale(coronal_data))
+        img_save3GrayScale(img, 'static', 'sagittal.jpg', 'axial.jpg', 'coronal.jpg')
 
         if request.method == 'POST':
             session['seg_model'] = request.form.get('seg_model')
@@ -89,18 +63,17 @@ def display():
 def segmentation():
     print(session['seg_model'])
     if session['seg_model'] == 'SynthSeg':
-        tigersyn.run('s', os.path.join('files', '*.nii.gz'), r'static')
+        tigersyn.run('s', os.path.join(app.config['UPLOAD_FOLDER'], '*.nii.gz'), r'static')
 
         mask = reorder_img(
             nib.load(os.path.join('static', 'image_syn.nii.gz')),
             resample='nearest').get_fdata()
+        
         mask_to_3img(mask, 'static', 'gist_ncar')
 
     # elif session['seg_model'] == 'Hippocampus':
     #     print('hipipipipipipip')
     #     tigersyn.run('h', os.path.join('files', '*.nii.gz'), r'static')
-
-
     return render_template('segmentation.html', raw_img_fname=session['img_fname'])
 
 
