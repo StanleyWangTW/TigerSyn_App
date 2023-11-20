47c9ade8673f2ed9dfdb3ff53bd1a29e41214acb
OuO fox <a20904662@gmail.com>
2023-11-21 05:59:59 +0800

    add two choice model and nilhearn mri display

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template, redirect, request, session, url_for
 import nibabel as nib
 from nilearn.image import reorder_img
@@ -77,36 +76,156 @@ def logout():
 @app.route("/patient=<patient_id>/show", methods=['GET', 'POST'])
 def show(patient_id):
     if 'img_fname' in session:
-        img = reorder_img(
-            nib.load(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz')),
-            resample='nearest').get_fdata()
+        #img = reorder_img(
+        #    nib.load(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz')),
+        #    resample='nearest').get_fdata()
 
-        img_save3GrayScale(img, 'static', 'sagittal.jpg', 'axial.jpg', 'coronal.jpg')
+        #img_save3GrayScale(img, 'static', 'sagittal.jpg', 'axial.jpg', 'coronal.jpg')
 
         if request.method == 'POST':
             session['seg_model'] = request.form.get('seg_model')
             return redirect(url_for('segmentation', patient_id=session['patient_id']))
         else:
-            return render_template('display.html', raw_img_fname=session['img_fname'])
+            raw_img_fname = session['img_fname']
+            html1 = f'''
+{{% extends "upload.html" %}}
+
+{{% block show_image %}}
+
+
+  <h1 class="text-center">{raw_img_fname}</h1>
+  <p></p>
+  <h1 class="text-center">MRI Image</h1>
+'''
+            html2 = view1image(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz'))
+            html3 = '''
+  <hr>
+  <form method="POST" class="img-thumbnail">
+    <h3>選擇執行的模型：</h3>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault1" value="SynthSeg" checked>
+      <label class="form-check-label" for="flexRadioDefault1">
+        SynthSeg
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault2" value="Hippocampus">
+      <label class="form-check-label" for="flexRadioDefault2">
+        Hippocampus
+      </label>
+    </div>
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary">確定分割</button>
+    </div>
+  </form>
+  {% block show_mask %}{% endblock %}
+{% endblock %}            
+'''
+            return render_template_string(html1 + html2 + html3)
+           # return render_template('display.html', raw_img_fname=session['img_fname'])
     else:
         return redirect(url_for('home'))
 
 @app.route("/patient=<patient_id>/segmentation", methods=['GET', 'POST'])
 def segmentation(patient_id):
     if session['seg_model'] == 'SynthSeg':
+        print("SynthSeg")
         tigersyn.run('s', os.path.join(app.config['UPLOAD_FOLDER'], '*.nii.gz'), r'static')
 
-        mask = reorder_img(
-            nib.load(os.path.join('static', 'image_syn.nii.gz')),
-            resample='nearest').get_fdata()
+        #mask = reorder_img(
+        #    nib.load(os.path.join('static', 'image_syn.nii.gz')),
+        #    resample='nearest').get_fdata()
         
-        mask_to_3img(mask, 'static', 'gist_ncar')
+        #mask_to_3img(mask, 'static', 'gist_ncar')
 
     # elif session['seg_model'] == 'Hippocampus':
     #     print('hipipipipipipip')
     #     tigersyn.run('h', os.path.join('files', '*.nii.gz'), r'static')
-    return render_template('segmentation.html', raw_img_fname=session['img_fname'])
+        raw_img_fname = session['img_fname']
+        html1 = f'''
+        {{% extends "upload.html" %}}
+    
+        {{% block show_image %}}
+    
+    
+          <h1 class="text-center">{raw_img_fname}</h1>
+          <p></p>
+          <h1 class="text-center">MRI Image</h1>
+        '''
+        html2 = view1image(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz'))
+        html3 = '''
+          <hr>
+          <form method="POST" class="img-thumbnail">
+            <h3>選擇執行的模型：</h3>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault1" value="SynthSeg" checked>
+              <label class="form-check-label" for="flexRadioDefault1">
+                SynthSeg
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault2" value="Hippocampus">
+              <label class="form-check-label" for="flexRadioDefault2">
+                Hippocampus
+              </label>
+            </div>
+            <div class="col-12">
+              <button type="submit" class="btn btn-primary">確定分割</button>
+            </div>
+          </form>
+        '''
+        html4 = view1image(os.path.join('static', 'image_syn.nii.gz'))
+        html5 = '''
+          {% block show_mask %}{% endblock %}
+        {% endblock %}            
+        '''
+        return render_template_string(html1 + html2 + html3 + html4 + html5)
+    #return render_template('segmentation.html', raw_img_fname=session['img_fname'])
+    if session['seg_model'] == 'Hippocampus':
+        print("Hippocampus")
+        input_data,header = turnDataToInputData(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz'))
+        output = predict("hippo.onnx", input_data, False)
+        save_pred = nib.nifti1.Nifti1Image(output.squeeze(), None, header=header)
+        nib.save(save_pred, os.path.join('static', "hippo.onnx_image.nii.gz"))
+        raw_img_fname = session['img_fname']
+        html1 = f'''
+                {{% extends "upload.html" %}}
 
+                {{% block show_image %}}
+
+
+                  <h1 class="text-center">{raw_img_fname}</h1>
+                  <p></p>
+                  <h1 class="text-center">MRI Image</h1>
+                '''
+        html2 = view1image(os.path.join(app.config['UPLOAD_FOLDER'], 'image.nii.gz'))
+        html3 = '''
+                  <hr>
+                  <form method="POST" class="img-thumbnail">
+                    <h3>選擇執行的模型：</h3>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault1" value="SynthSeg" checked>
+                      <label class="form-check-label" for="flexRadioDefault1">
+                        SynthSeg
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="seg_model" id="flexRadioDefault2" value="Hippocampus">
+                      <label class="form-check-label" for="flexRadioDefault2">
+                        Hippocampus
+                      </label>
+                    </div>
+                    <div class="col-12">
+                      <button type="submit" class="btn btn-primary">確定分割</button>
+                    </div>
+                  </form>
+                '''
+        html4 = view1image(os.path.join('static', "hippo.onnx_image.nii.gz"))
+        html5 = '''
+                  {% block show_mask %}{% endblock %}
+                {% endblock %}            
+                '''
+        return render_template_string(html1 + html2 + html3 + html4 + html5)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tools.py
+++ tools.py
@@ -3,7 +3,13 @@ import os
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
+from nilearn import plotting, image
+from flask import render_template_string
+from nilearn.image import reorder_img
+import onnxruntime as ort
+import torch
+import torch.nn
+import nibabel as nib
 def to_grayscale(img):
     return ((img - img.min()) / (img.max() - img.min()) * 255).astype(np.uint8)
 
@@ -34,3 +40,47 @@ def mask_to_3img(mask, dir, cmap):
     plt.imshow(np.rot90(mask[:, 100, :]), cmap=cmap)
     plt.axis('off')
     plt.savefig(os.path.join(dir, 'coronal_mask.jpg'), bbox_inches='tight')
+
+def view1image(path1):
+    nii_file1 = path1
+    img1 = image.load_img(nii_file1)
+    view = plotting.view_img(img1, black_bg=True, colorbar=False, bg_img=False)
+    html_content = view.get_iframe()
+    html = f'<br><html><body>{html_content}</body></html>'
+    return render_template_string(html)
+
+def view2image(path1, path2):
+    nii_file1 = path1  # 更換為您檔案的路徑
+    nii_file2 = path2
+    img1 = image.load_img(nii_file1)
+    img2 = image.load_img(nii_file2)
+    view = plotting.view_img(img1, black_bg=True, colorbar=False, bg_img=False)
+    view2 = plotting.view_img(img2, black_bg=True, colorbar=False, bg_img=False)
+    html_content = view.get_iframe()
+    html_content2 = view2.get_iframe()
+    html = f'<br><html><body>{html_content}<br>{html_content2}</body></html>'
+    return render_template_string(html)
+
+def turnDataToInputData(file_path):
+    origin = nib.load(file_path)
+    copy_header = origin.header.copy()
+    origin = reorder_img(origin, resample="continuous")
+    data = origin.get_fdata()
+    data = np.expand_dims(data, axis=0)
+    data = np.expand_dims(data, axis=0)
+    return data,copy_header
+
+def predict(model, data, GPU):
+    """read array-like data, then segmentation"""
+    if GPU and (ort.get_device() == "GPU"):
+        ort_sess = ort.InferenceSession(model, providers=['CUDAExecutionProvider'])
+    else:
+        ort_sess = ort.InferenceSession(model, providers=["CPUExecutionProvider"])
+    data_type = 'float32'
+
+    sigmoid = torch.nn.Sigmoid()
+    out_sig = sigmoid(torch.tensor(ort_sess.run(None, {'input': data.astype(data_type)})[0]))
+    output = out_sig.numpy()
+    output[output > 0.5] = 1
+    output[output <= 0.5] = 0
+    return output
